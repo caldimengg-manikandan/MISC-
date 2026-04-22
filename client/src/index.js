eb252981cd2c8f8c475fdd7cc8438142f0314434
VigneshGovardhan <vigneshgovardhan5163@gmail.com>
2026-04-22 15:17:13 +0530

    fix: add basename='/misc' to Router to fix subfolder hosting routing issues

--- client/src/index.js
+++ client/src/index.js
@@ -16,7 +16,7 @@ async function init() {
   
   root.render(
     <React.StrictMode>
-      <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <Router basename="/misc" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <AuthProvider>
           <EstimationProvider>
             <App />
